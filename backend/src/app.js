e9c2d2d03139322437f9373b428fb8879e54f908
Sarah Ferreira <sarahfssilva@hotmail.com>
2020-06-01 23:32:22 -0300

    omnistack :)

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const {errors} = require('celebrate')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+app.use(errors())
+
+module.exports = app
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação no back-end;
+ * POST: Criar uma informação no back-end;
+ * PUT: Alterar uma informação no back-end;
+ * DELETE: Deletar uma informação no back-end.
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+  */
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './global.css'
+import Routes from './routes'
+
+// JSX Javascript XML
+
+export default function App() {
+  return (
+      <Routes/>
+  );
+}
+
